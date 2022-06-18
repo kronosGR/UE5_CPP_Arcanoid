604631e08c24686a67e27f48785f8aa8cc79501f
Katsanakis Georgios <geo.elgeo@gmail.com>
2022-06-18 20:37:33 +0200

    Initial commit

---
+++ Source/UE5_CPP_Arcanoid/UE5_CPP_ArcanoidGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE5_CPP_ArcanoidGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE5_CPP_ARCANOID_API AUE5_CPP_ArcanoidGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
